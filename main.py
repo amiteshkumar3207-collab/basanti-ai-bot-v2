1e4b63ce78f1d2934b9aac75cc067a029758e2bb
amiteshkumar3207-collab <amiteshkumar3207@gmail.com>
2026-02-09 00:57:29 +0400

    Update main.py

--- main.py
+++ main.py
@@ -4,11 +4,15 @@ import os
 TOKEN = os.getenv("TELEGRAM_TOKEN")
 
 async def start(update, context):
-    await update.message.reply_text("Hello 👋 Bot alive hai")
+    await update.message.reply_text("Hello 👋 Bot alive")
 
 app = Application.builder().token(TOKEN).build()
 app.add_handler(CommandHandler("start", start))
 
 if __name__ == "__main__":
     print("Bot started")
-    app.run_polling()
+    app.run_webhook(
+        listen="0.0.0.0",
+        port=int(os.getenv("PORT", 8080)),
+        webhook_url=os.getenv("WEBHOOK_URL")
+    )
