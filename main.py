12749f7bd071702d858a48149779d19969211bb1
amiteshkumar3207-collab <amiteshkumar3207@gmail.com>
2026-02-09 00:32:14 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,14 @@
-from telegram.ext import Application, CommandHandler, MessageHandler, filters
+from telegram.ext import Application, CommandHandler
 import os
-from ai_engine import ask_ai
 
 TOKEN = os.getenv("TELEGRAM_TOKEN")
 
-if not TOKEN:
-    print("TELEGRAM_TOKEN missing")
-    exit(1)
-
 async def start(update, context):
-    await update.message.reply_text("Basanti AI Bot alive 🤖")
-
-async def chat(update, context):
-    reply = ask_ai(update.message.text)
-    await update.message.reply_text(reply)
+    await update.message.reply_text("Hello 👋 Bot alive hai")
 
 app = Application.builder().token(TOKEN).build()
-
 app.add_handler(CommandHandler("start", start))
-app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, chat))
+
+if __name__ == "__main__":
+    print("Bot started")
+    app.run_polling()
