2e4e00a15efe9d5e73f39219b2f7a9b77c07d50e
amiteshkumar3207-collab <amiteshkumar3207@gmail.com>
2026-02-08 20:07:08 +0400

    Update memory.py

--- memory.py
+++ memory.py
@@ -1,1 +1,11 @@
 
+# memory.py
+# Simple in-memory storage (safe for STEP 2)
+
+user_memory = {}
+
+def set_name(user_id, name):
+    user_memory[user_id] = name
+
+def get_name(user_id):
+    return user_memory.get(user_id)
